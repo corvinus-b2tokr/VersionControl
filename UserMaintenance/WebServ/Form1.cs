5c77a879cbf8a4581d2e534f26cc3a778580dac9
Olivér Zoltán Karsai <oliver.karsai@stud.uni-corvinus.hu>
2021-10-28 11:44:21 +0200

    #5 kész

--- UserMaintenance/WebServ/Form1.cs
+++ UserMaintenance/WebServ/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
 using WebServ.Entities;
 using WebServ.MnbServiceReference;
 
@@ -15,11 +16,13 @@ namespace WebServ
     public partial class Form1 : Form
     {
         BindingList<RateData> Rates = new BindingList<RateData>();
+        string result { get; set; }
         public Form1()
         {
             InitializeComponent();
             UseWeb();
             dataGridView1.DataSource = Rates;
+            XML();
         }
 
         private void UseWeb()
@@ -32,8 +35,29 @@ namespace WebServ
                 endDate = "2020-06-30"
             };
             var response = mnbService.GetExchangeRates(request);
-            var result = response.GetExchangeRatesResult;
-            Console.WriteLine(result);
+            var eredm = response.GetExchangeRatesResult;
+            result = eredm;
+        }
+
+        private void XML()
+        {
+            var xml = new XmlDocument();
+            xml.LoadXml(result);
+
+            foreach (XmlElement element in xml.DocumentElement)
+            {
+                var rate = new RateData();
+                Rates.Add(rate);
+
+                rate.Date = DateTime.Parse(element.GetAttribute("date"));
+
+                var childElement = (XmlElement)element.ChildNodes[0];
+                rate.Currency = childElement.GetAttribute("curr");
+
+                var unit = decimal.Parse(childElement.GetAttribute("unit"));
+                var value = decimal.Parse(childElement.InnerText);
+                if (unit != 0) rate.Value = value / unit;
+            }
         }
     }
 }
