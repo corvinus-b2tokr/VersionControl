07e98035539101f62d4e37486db096ee8b2e9011
Olivér Zoltán Karsai <oliver.karsai@stud.uni-corvinus.hu>
2021-10-28 12:11:37 +0200

    #7 kész

--- UserMaintenance/WebServ/Form1.cs
+++ UserMaintenance/WebServ/Form1.cs
@@ -21,10 +21,8 @@ namespace WebServ
         public Form1()
         {
             InitializeComponent();
-            UseWeb();
             dataGridView1.DataSource = Rates;
-            XML();
-            Diagram();
+            RefreshData();
         }
 
         private void UseWeb()
@@ -32,9 +30,9 @@ namespace WebServ
             var mnbService = new MNBArfolyamServiceSoapClient();
             var request = new GetExchangeRatesRequestBody()
             {
-                currencyNames = "EUR",
-                startDate = "2020-01-01",
-                endDate = "2020-06-30"
+                currencyNames = comboBox1.SelectedItem.ToString(),
+                startDate = dateTimePicker1.Value.ToString(),
+                endDate = dateTimePicker2.Value.ToString()
             };
             var response = mnbService.GetExchangeRates(request);
             var eredm = response.GetExchangeRatesResult;
@@ -77,6 +75,29 @@ namespace WebServ
             chartArea.AxisX.MajorGrid.Enabled = false;
             chartArea.AxisY.MajorGrid.Enabled = false;
             chartArea.AxisY.IsStartedFromZero = false;
+        }
+
+        private void RefreshData()
+        {
+            Rates.Clear();
+            UseWeb();
+            XML();
+            Diagram();
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            RefreshData();
+        }
+
+        private void dateTimePicker2_ValueChanged(object sender, EventArgs e)
+        {
+            RefreshData();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            RefreshData();
         }
     }
 }
