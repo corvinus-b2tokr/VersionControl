3897e13ac2df950b70cdaacacdb53f7f3d6d6991
Olivér Zoltán Karsai <oliver.karsai@stud.uni-corvinus.hu>
2021-11-11 13:18:34 +0100

    termékválasztó gombok kész

--- UserMaintenance/FejlMint/Form1.cs
+++ UserMaintenance/FejlMint/Form1.cs
@@ -1,4 +1,5 @@
-﻿using FejlMint.Entities;
+﻿using FejlMint.Abstractions;
+using FejlMint.Entities;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -13,13 +14,17 @@ namespace FejlMint
 {
     public partial class Form1 : Form
     {
-        List<Ball> _balls = new List<Ball>();
+        List<Toy> _toys = new List<Toy>();
+        private Toy _nextToy;
 
-        private BallFactory _factory;
-        public BallFactory Factory
+        private IToyFactory _factory;
+        public IToyFactory Factory
         {
             get { return _factory; }
-            set { _factory = value; }
+            set {
+                _factory = value;
+                DisplayNext();
+            }
         }
 
         public Form1()
@@ -30,26 +35,45 @@ namespace FejlMint
 
         private void createTimer_Tick(object sender, EventArgs e)
         {
-            var ball = Factory.CreateNew();
-            _balls.Add(ball);
-            ball.Left = -ball.Width;
-            mainPanel.Controls.Add(ball);
+            var toy = Factory.CreateNew();
+            _toys.Add(toy);
+            toy.Left = -toy.Width;
+            mainPanel.Controls.Add(toy);
         }
 
         private void conveyorTimer_Tick(object sender, EventArgs e)
         {
             var maxPosition = 0;
-            foreach (var ball in _balls)
+            foreach (var toy in _toys)
             {
-                ball.MoveBall();
-                if (ball.Left > maxPosition) maxPosition = ball.Left;
+                toy.MoveToy();
+                if (toy.Left > maxPosition) maxPosition = toy.Left;
             }
             if (maxPosition > 1000)
             {
-                var oldestBall = _balls[0];
-                mainPanel.Controls.Remove(oldestBall);
-                _balls.Remove(oldestBall);
+                var oldestToy = _toys[0];
+                mainPanel.Controls.Remove(oldestToy);
+                _toys.Remove(oldestToy);
             }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Factory = new CarFactory();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Factory = new BallFactory();
+        }
+
+        private void DisplayNext()
+        {
+            if (_nextToy != null) Controls.Remove(_nextToy);
+            _nextToy = Factory.CreateNew();
+            _nextToy.Left = label1.Left + label1.Width + 10;
+            _nextToy.Top = label1.Top - 20;
+            Controls.Add(_nextToy);
         }
     }
 }
